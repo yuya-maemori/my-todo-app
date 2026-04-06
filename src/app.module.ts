aba887e7e755931f70048efd18270fe8037585e4
yuya-maemori <yuya.maemori@vitalize.co.jp>
2026-04-06 20:41:42 +0900

    Task6:HelloWorld エンドポイントを作る

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HelloModule],
   controllers: [AppController],
   providers: [AppService],
 })
